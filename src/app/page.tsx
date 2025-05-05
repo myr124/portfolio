baf173af7c2325839ea5c4bfc45b4a08a0e90d68
George <eric.george.ext@siemens-energy.com>
2025-05-05 14:55:14 -0400

    trying to make cube rotate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 'use client'
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import React, { useRef, useState } from "react";
 
-export default function Home() {
+
+const MyMesh = () => {
     const meshMat = React.useRef(null)
     const [color, setcolor] = useState("blue")
+    useFrame(() => {
+        meshMat.current.rotation.x = 0.1
+    })
+    return (
+        <mesh onClick={() => { setcolor("red") }}>
+            <boxGeometry position={[0, 0, 0]} args={[10, 10, 10]}></boxGeometry>
+            <meshStandardMaterial ref={meshMat} color={color}  ></meshStandardMaterial>
+        </mesh>
+    )
+}
+
+
+export default function Home() {
     return (
         <div>
-            <Canvas camera={{ position: [10, 0, 10] }}>
-                <mesh onClick={() => { setcolor("red") }}>
-                    <boxGeometry position={[0, 0, 0]} args={[10, 10, 10]}></boxGeometry>
-                    <meshStandardMaterial ref={meshMat} color={color}  ></meshStandardMaterial>
-                </mesh>
+            <Canvas camera={{ position: [10, 10, 10] }}>
+                <MyMesh></MyMesh>
                 <ambientLight intensity={0.9} />
                 <directionalLight color="red" position={[0, 0, 5]} />
             </Canvas>
