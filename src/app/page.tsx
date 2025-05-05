18d0160eb12eacce913d61ebd5948ef17dedcd36
George <eric.george.ext@siemens-energy.com>
2025-05-05 14:29:43 -0400

    basic testing with react three fiber and three js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+'use client'
+
+import { Canvas } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
 
 export default function Home() {
+    const meshMat = React.useRef(null)
+    const [color, setcolor] = useState("blue")
     return (
         <div>
-            Testing...
+            <Canvas camera={{ position: [10, 0, 10] }}>
+                <mesh onClick={() => { setcolor("red") }}>
+                    <boxGeometry position={[0, 0, 0]} args={[10, 10, 10]}></boxGeometry>
+                    <meshStandardMaterial ref={meshMat} color={color}  ></meshStandardMaterial>
+                </mesh>
+                <ambientLight intensity={0.9} />
+                <directionalLight color="red" position={[0, 0, 5]} />
+            </Canvas>
         </div>
     );
 }
